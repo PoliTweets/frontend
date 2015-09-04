ed975db0e7d358ccabcf15128fd5e4d47c692a72
fbenoit <b@redsteammultimedia.com>
2015-09-04 17:25:42 +0200

    added orientation screen
    orientation.html

--- js/main.js
+++ js/main.js
@@ -1,1 +1,38 @@
+$(document).ready(function() {
+    $('map').imageMapResize();
+    
+   
+    $('#left').click(function (e) {
+    	e.preventDefault();
+		setOrientation(0);
+	});
+	
+	
+    $('#center').click(function (e) {
+    	e.preventDefault();
+		setOrientation(1);
+	});
 
+    $('#right').click(function (e) {
+    	e.preventDefault();
+		setOrientation(2);
+	});
+    
+    var currentOrientation = -1;
+    var baseImage = $('#parlementImg').attr('src');
+    
+    function setOrientation(value) {
+    
+    	if (value== currentOrientation) value = -1;
+    	
+    	var targetImg = baseImage;
+		if (value>-1) {
+			targetImg = $('#parlementImg').attr('type'+value);
+		}
+		
+		currentOrientation = value;
+				
+		$('#parlementImg').attr('src', targetImg);
+    }
+    
+});
